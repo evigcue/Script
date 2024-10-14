b2d94b7d8a50c554bccc1100511d10c1ae1c4906
evigcue <149626333+evigcue@users.noreply.github.com>
2024-10-14 13:01:56 +0200

    mejoras del sh y prueba html

--- arrays_asociativos/scarrayasoc1.sh
+++ arrays_asociativos/scarrayasoc1.sh
@@ -12,32 +12,33 @@ for key in "${!colores[@]}"; do
     echo "$key"
 done
 
-read -rp "¿Qué color quieres para el fondo de la página?" backcolor
-read -rp "¿Qué color quieres para el parrafo de la página?" pcolor
-read -rp "¿Qué color quieres para el texto de la página?" textcolor
+read -rp "¿Qué color quieres para el fondo de la página?: " backcolor
+read -rp "¿Qué color quieres para el parrafo de la página?: " pcolor
+read -rp "¿Qué color quieres para el texto de la página?: " textcolor
 
-echo "<style>
-    *{
-        background-color: ${color[$backcolor]};
+ip=$(ifconfig)
 
-        p{
-        background-color: ${color[$pcolor];
-        color: ${color[$textcolor]};
-        }
-    } 
-</style>" >> style_esteban.html
+if [ -f "index_esteban.html" ]; then
+    rm "index_esteban.html"
+fi
 
-ip=ifconfig
+cat > "index_esteban.html" <<EOF
+<!DOCTYPE html>
+    <head>
+        <style>
+            *{
+                background-color: ${color[$backcolor]};
 
-echo "<!DOCTYPE html>
-<html lang='en'>
-<head>
-    <link rel='stylesheet' href='style_esteban.css'>
-    <meta charset='UTF-8'>
-    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-    <title>Document</title>
-</head>
-<body>
-    $ip
-</body>
-</html>" >> index_esteban.html
+                p{
+                    background-color: ${color[$pcolor]};
+                    color: ${color[$textcolor]};
+                }
+            } 
+        </style>
+        <title>pagina</title>
+    </head>
+    <body>
+        $ip
+    </body>
+</html>
+EOF
